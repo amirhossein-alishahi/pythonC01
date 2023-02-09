283e91330096aa1d70467ed3ee293bd2715c0f04
AmirHossein Alishahi <59320577+amirhossein-alishahi@users.noreply.github.com>
2023-02-09 20:55:26 +0330

    second session added to git

---
+++ second session/second session.py
@@ -0,0 +1,86 @@
+#%% Variables name
+
+#okay
+_var = ""
+var = ""
+va23r = ""
+var1 = ""
+VAR2 = ""
+Var = ""
+first_var = ""
+
+# illegal variable name
+# 2va = ""
+# first var = ""
+# first-var = ""
+
+#Camel Case
+firstVariable = ""
+#Snake Case
+first_variable = ""
+
+#%% Data type
+
+integer = 23
+floatNumber = 23.3
+stringVar = "text"
+
+
+#%% operators
+
+num1 = 10
+num2 = 20
+
+sum = num1 + num2
+
+num = 10 + 20
+num = 20 - 10
+num = 20 / 10
+num = 22 // 10 
+num = 20 * 10
+num = 20 % 10
+
+num += 10
+num = num + 10
+
+num -= 10
+num = num - 10
+
+num /= 10
+num = num / 10
+
+num //= 10
+num = num // 10
+
+num *= 10
+num = num * 10
+
+num = num % 10
+num %= 10
+
+#%% rounding
+
+num = 23.33333333333
+num = round(num,3)
+
+
+#%% casting
+
+num = "3232.23"
+
+num = float(num)
+
+num = int(num)
+
+num = str(num)
+
+
+#%% comment
+
+"""
+Multi-line
+comments
+"""
+
+# One line comments
+
